fa06d20de7d22f771018960698d8f89e68e64640
Alexey1307 <bachurin1307@gmail.com>
2022-05-12 14:33:59 +0300

    php

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Mail;
+use App\Models\Booking;
+
 
 class mailController extends Controller
 {
     public function send(){
-        Mail::send(['text' => 'mail'], ['name', 'Hizhina'], function($message){
+        // Mail::send(['text' => 'mail'], ['name', 'Hizhina'], function($message){
+        // Mail::send('mail', ['name', 'Hizhina'], function($message){
+        Mail::send('mail', ['data' => Booking::all()], function($message){
             $message->to('hizhinaHotel@yandex.ru', 'Дорогому гостю')->subject('Бронирование');
             $message->from('hizhinaHotel@yandex.ru', 'Хижина');
         });
+
+        return view('messages');
     }
 }
--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,4 +1,30 @@
 Вы забронировали номер в Хижине!
- с oneData инфу.
+
+
+@foreach ($data as $item)
+    <div>
+        <h3>Дата заезда: 
+            <span style="color: green">{{ $item->enterDate }}</span>
+        </h3>
+        <h3>Дата выезда: 
+            <span style="color: green">{{ $item->outDate }}</span>
+        </h3>
+        <h3>Количество номеров: 
+            <span style="color: green">{{ $item->countRooms }}</span>
+        </h3>
+        <h3>Предоплата: 
+            <span style="color: green">{{ $item->prepayment }}</span>
+        </h3>
+        <h3>Остаток к оплате: <span style="color: green">{{ $item->payment }}</span>
+        </h3>
+    </div>
+@endforeach
+
+<h1>Хижина</h1>
+    <a href="tel:89184289606">8-918-428-96-06</a><br><br>
+    <a href="https://t.me/hizhinaHotel" style="padding-right: 2em">Telegram</a>
+    <a href="https://vk.com/club210847892">VK</a
+
+
 
  
