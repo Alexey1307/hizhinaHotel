7245f5f25decb42b6528eb227ad7720a7ec85c17
Alexey1307 <bachurin1307@gmail.com>
2022-07-17 12:57:00 +0300

    commit

--- public/js/createCalendar.js
+++ public/js/createCalendar.js
@@ -1,11 +1,25 @@
-const monthsName = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-    'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-let months = document.querySelector('.months');
-let years = document.querySelector('.years');
+const monthsName = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь"
+];
+let months = document.querySelector(".months");
+let years = document.querySelector(".years");
 
 let curentDate = new Date();
 let curentYear = curentDate.getFullYear();
 let curentMonth = curentDate.getMonth();
+
+let choice = new Set();
 let d = new Date();
 
 function createCalendar(elem, year, month) {
@@ -26,22 +40,27 @@ function createCalendar(elem, year, month) {
         }
     }
 
-    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+    let table =
+        "<table><thead><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr></thead>";
+
+    table += `<tbody>`;
+
     let input = '<input class="input" type="text" value="3">';
 
     // пробелы для первого ряда
     // с понедельника до первого дня месяца
     for (let i = 0; i < getDay(d); i++) {
-        table += '<td></td>';
+        table += "<td></td>";
     }
 
     // <td> ячейки календаря с датами
     while (d.getMonth() == mon) {
         let findDay = d.getDate();
-        table += '<td>' + findDay + input + '</td>';
-            if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
-                table += '</tr><tr>';
-            }
+        table += `<td data-day="${findDay}" data-mouth="${mon}">${findDay} ${input}</td>`;
+        if (getDay(d) % 7 == 6) {
+            // вс, последний день - перевод строки
+            table += "</tr><tr>";
+        }
         d.setDate(d.getDate() + 1);
     }
 
@@ -49,12 +68,13 @@ function createCalendar(elem, year, month) {
     // 29 30 31 * * * *
     if (getDay(d) != 0) {
         for (let i = getDay(d); i < 7; i++) {
-            table += '<td></td>';
+            table += "<td></td>";
         }
     }
 
+    table += `<tbody>`;
     // закрыть таблицу
-    table += '</tr></table>';
+    table += "</tr></table>";
     elem.innerHTML = table;
 }
 
@@ -66,42 +86,42 @@ function getDay(date) {
 
 createCalendar(calendar, curentYear, curentMonth - 1);
 
-
 //реализация переключения стрелок
 function switchMounth() {
-    let prev = document.querySelector('.prev');
-    let next = document.querySelector('.next');
+    let prev = document.querySelector(".prev");
+    let next = document.querySelector(".next");
 
-
-    prev.addEventListener('click', () => {
+    prev.addEventListener("click", () => {
         if (curentMonth == 0) {
             curentYear -= 1;
-        }
-        else if (curentMonth == curentDate.getMonth()
-            && curentYear == curentDate.getFullYear()) {
+        } else if (
+            curentMonth == curentDate.getMonth() &&
+            curentYear == curentDate.getFullYear()
+        ) {
             prev.disabled = true;
-        }
-        else if (curentMonth == (curentDate.getMonth() + 1)
-            && curentYear == curentDate.getFullYear()) {
+        } else if (
+            curentMonth == curentDate.getMonth() + 1 &&
+            curentYear == curentDate.getFullYear()
+        ) {
             prev.disabled = true;
         }
         curentMonth = (12 + curentMonth - 1) % 12;
         createCalendar(calendar, curentYear, curentMonth);
         months.innerHTML = monthsName[curentMonth];
         years.innerHTML = curentYear;
+        choice = new Set();
+    });
 
-    })
-
-    next.addEventListener('click', () => {
+    next.addEventListener("click", () => {
         curentMonth++;
         prev.disabled = false;
+        choice = new Set();
 
         for (let i = monthsName.length; i >= 0; i--) {
             if (i == curentMonth) {
                 createCalendar(calendar, curentYear, curentMonth);
                 months.innerHTML = monthsName[i];
-            }
-            else if (curentMonth > 11) {
+            } else if (curentMonth > 11) {
                 curentYear += 1;
                 curentMonth = 0;
 
@@ -110,64 +130,67 @@ function switchMounth() {
                 years.innerHTML = curentYear;
             }
         }
-    })
+    });
 }
 
 switchMounth();
 
-
 // ***Colors part***
 
 const colors = {
-    '3': 'green',
-    '2': 'green',
-    '1': 'green',
-    '0': 'red',
+    3: "green",
+    2: "green",
+    1: "green",
+    0: "red"
 };
 
-const setColor = input => {
+const setColor = (input) => {
     const { value } = input;
     if (colors.hasOwnProperty(value)) {
         input.style.backgroundColor = colors[value];
     }
-}
+};
 
-const onInput = e => {
+const onInput = (e) => {
     setColor(e.target);
-}
+};
 
-let rooms = document.querySelectorAll('.input');
-rooms.forEach(elem => {
-    elem.addEventListener('input', onInput);
+let rooms = document.querySelectorAll(".input");
+rooms.forEach((elem) => {
+    elem.addEventListener("input", onInput);
     setColor(elem);
 });
-
-
 
 //Реализуем выбор дат
 function choiceDates() {
     let arrDates = [];
     let arr = [];
-    let arr2 = [];
     let classList = document.body.classList;
     let firstDay;
     let lastDay;
     calendar.addEventListener("click", (e) => {
-        if (e.target.innerText>=1){
-            arrDates.push(e.target.innerText); // + curentMonth
-            firstDay = +arrDates[0];
-            lastDay = +arrDates[arrDates.length - 1];
-            
-            for(let i = firstDay; i<= lastDay; i++){
-                arr2.push(i);
-                
+        let tar = e.target;
+        if (tar.nodeName === "TD") {
+            let selectDay = +tar.dataset.day;
+
+            if (choice.size === 0 || choice.size > 1) {
+                [...document.querySelectorAll("td.yellow")].forEach((td) =>
+                    td.classList.remove("yellow")
+                );
+                choice = new Set();
+                choice.add(selectDay);
+                e.target.classList.add("yellow");
+            } else if (choice.size === 1) {
+                let firstSelect = Math.min([...choice][0], selectDay);
+                let secondSelect = Math.max([...choice][0], selectDay);
+                for (let i = 0; firstSelect + i <= secondSelect; i++) {
+                    document
+                        .querySelector(`td[data-day="${firstSelect + i}"]`)
+                        .classList.add("yellow");
+                    choice.add(firstSelect + i);
+                }
             }
-            console.log(arr2);
-
-            
-
-            e.target.classList.toggle('yellow');
-
+            console.log(choice);
         }
     });
 }
